588b97a64d53601b192be93523e5f1448c07cd4d
Abby Ruffier <56894920+Aruffier88@users.noreply.github.com>
2021-02-23 20:41:47 -0500

    Week 5 Arrays

---
+++ week5lab/js/array.js
@@ -0,0 +1,18 @@
+//week 5 Lab Abby Ruffier 2/23/21 WDD221
+
+let books =["Harry Potter", "The Shining", "War and Peace", "The Great Gatsby",];
+
+console.log(books + '\n'); 
+
+console.log(books.length + '\n');
+
+console.log(books[3]);
+
+{
+document.write(books[i] + “<br>”);
+
+}
+
+letascendingBooks = books.sort();
+
+let vegetables = new Array('corn','bean','broccoli','spinach','celery','cabbage','kale');
